0878cfd97757ea6de10cbe4cdd0df8907f53e648
Amrit Ambastha <97228720+amritambastha4@users.noreply.github.com>
2024-07-17 21:30:14 +0530

    initial-commit

---
+++ Amrit-portfolio-master/src/constants.js
@@ -0,0 +1,61 @@
+
+export const services = [
+    {
+        title: "Technology Enthusiast",
+        icon: 'https://www.pngarts.com/files/3/Technology-PNG-High-Quality-Image.png',
+    },
+    {
+        title: "Web Developer",
+        icon: 'https://static-00.iconduck.com/assets.00/web-developer-illustration-1004x1024-wcqgbag3.png',
+    },
+    {
+        title: "UI-UX Designer",
+        icon: 'https://static.vecteezy.com/system/resources/previews/009/992/271/large_2x/ui-ux-icon-sign-design-free-png.png',
+    },
+];
+
+export const name = 'Amrit';
+
+export const experiences = [
+    
+    {
+        'company': 'Edvedha',
+        'role': 'Internet of things',
+        'duration': 'June 2024 - Present',
+        'logo': 'https://edvedha.com/images/logo/edlogo.png',
+        'points': [
+            ''
+        ],
+        'url': '',
+    },
+    {
+        'company': 'Bharat Sanchar Nigam Limited',
+        'role': 'Computer Networking',
+        'duration': 'May 2024 - June 2024',
+        'logo': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMUTPffwr8em2xeGwxJa6YAE_HVefAAGoM5w&s',
+        'points': [
+            'Involved in dynamic contributions to web and mobile📱 development projects, actively influencing and improving project outcomes.',
+            'Developed multiple shared components to simplify tasks for fellow developers, boosting efficiency and teamwork.',
+            'Enhanced product performance through significant contributions to backend development, optimizing essential components for improved efficiency 📈.',
+            'Implemented data security for the company’s product to provide role-based access control ⚙️.'
+        ],
+        'url': '',
+    },
+    {
+        'company': 'Tata Motors',
+        'role': 'Intern at Engine Factory',
+        'duration': 'May 2023 - June 2023',
+        'logo': 'https://th.bing.com/th/id/OIP.yevBCWXrDlra9iqRaZku4QHaEK?rs=1&pid=ImgDetMain',
+        'points': [
+            'Gained experience in block line control & contributing to the development and maintenance of fuel injector.',
+            'Worked on the application dashboard, maintaining the Excel sheet & making it more user-friendly and improving its overall functionality📱.'
+        ],
+        'url': '',
+    },
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = 'service_90wb9t5';
+export const EMAIL_JS_TEMPLATE_ID = 'template_8l87rew';
+export const EMAIL_JS_PUBLIC_KEY = 'pHPjc9RGGkF66r86N';
